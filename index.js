30f9958d5d96cc96a6c522eeb3acd5fadff286e4
Necralma <lihuel.almada@gmail.com>
2020-10-22 23:08:44 -0300

    used fs

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var lodash = require('lodash')
-var prod150 = require('./data/prod150.json')
+var prod150 = require('./data/products.json')
+var fs = require('fs');
 
 const prod150Filters = prod150.filter(e => e.price > 150);
 
-console.log('Lista filatrada: ',prod150Filters)
-console.log(lodash.shuffle(prod150Filters))
+fs.writeFile('./data/prod150.json', JSON.stringify(lodash.shuffle(prod150Filters)), () => {
+    console.log('Ready');
+    console.log('Lista filatrada: ',prod150Filters)
+    console.log(lodash.shuffle(prod150Filters))
+})
